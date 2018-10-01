8093fc2de6a2b09764ea1ce0faf731caa4b7b266
eightyfour <eightyfour@thinkuseful.de>
2018-10-01 23:24:16 +0200

    add lib expoter

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Cow from "./components/Cow.vue"
+import SelectCowFlowerMenu from "./components/SelectCowFlowerMenu.vue"
+
+export default {
+    Cow,
+    SelectCowFlowerMenu
+}
+export {
+    Cow,
+    SelectCowFlowerMenu
+}
